1aac9ae616ab39d650b639682a0a7383800580a6
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 03:12:47 +0800

    add controller

---
+++ PokemonTest/Controllers/PokemonController.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using PokemonTest.Interfaces;
+using PokemonTest.Models;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace PokemonTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet("api/pokemons")]
+        [ProducesResponseType(200, Type = typeof(ICollection<Pokemon>))]
+        public IActionResult GetAllPokemons()
+        {
+            var pokemons = _pokemonRepository.GetAllPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ PokemonTest/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonTest.Data;
+using PokemonTest.Interfaces;
+using PokemonTest.Models;
+
+namespace PokemonTest.Repository
+{
+    public class PokemonRepository :IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Pokemon> GetAllPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
