c16e9eb7c451f771f0ede5dfe1c30309d4b08e74
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 06:55:12 +0800

    categories interface

---
+++ PokemonTest/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonTest.Models;
+
+namespace PokemonTest.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetAllCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+
+    }
+}
