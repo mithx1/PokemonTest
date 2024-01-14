558fbc9b48891c165c672af9f04fedb6eb4b1ac0
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 06:56:08 +0800

    add CategoryController

---
+++ PokemonTest/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonTest.Dto;
+using PokemonTest.Interfaces;
+using PokemonTest.Models;
+using PokemonTest.Repository;
+
+namespace PokemonTest.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository _categoryRepository;
+        private IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("api/categories")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetAllCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetAllCategories());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(categories);
+        }
+
+        [HttpGet("api/categories/{id}")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCategory(int id)
+        {
+            if (!_categoryRepository.CategoryExists(id))
+                return NotFound();
+
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(id));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(category);
+        }
+    }
+}
