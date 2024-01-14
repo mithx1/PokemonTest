f2b762967134b3f961115baa3d9bb9043f904e57
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 06:55:28 +0800

    repository methods for category

---
+++ PokemonTest/Repository/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonTest.Data;
+using PokemonTest.Interfaces;
+using PokemonTest.Models;
+
+namespace PokemonTest.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private DataContext _context;
+
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(c => c.Id == id);
+        }
+
+        public ICollection<Category> GetAllCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            if(!CategoryExists(id))
+            {
+                return null;
+            }
+            return _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(e => e.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
+        }
+    }
+}
