ba857298c8db68fc3b7734bc8196cbe833ac48a7
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 04:57:36 +0800

    add mapping profile

---
+++ PokemonTest/Helpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonTest.Models;
+using PokemonTest.Dto;
+
+namespace PokemonTest.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
