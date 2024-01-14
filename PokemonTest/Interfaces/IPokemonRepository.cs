7b243de23067643141abf6ba3d6d68f14b4b15b0
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 04:57:09 +0800

    add repository

--- PokemonTest/Interfaces/IPokemonRepository.cs
+++ PokemonTest/Interfaces/IPokemonRepository.cs
@@ -5,5 +5,9 @@ namespace PokemonTest.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetAllPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int id);
+        bool PokemonExists(int id);
     }
 }
