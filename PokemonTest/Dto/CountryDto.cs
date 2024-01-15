dda94ce90500fba0bcac8f01cfed512794740db1
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 13:43:03 +0800

    dtos, mapping properties add service

---
+++ PokemonTest/Dto/CountryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonTest.Dto
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- PokemonTest/Helpers/MappingProfiles.cs
+++ PokemonTest/Helpers/MappingProfiles.cs
@@ -10,6 +10,8 @@ namespace PokemonTest.Helpers
         {
             CreateMap<Pokemon, PokemonDto>();
             CreateMap<Category, CategoryDto>();
+            CreateMap<Country, CountryDto>();
+            CreateMap<Owner, OwnerDto>();
         }
     }
 }
