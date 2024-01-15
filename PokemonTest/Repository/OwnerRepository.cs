c1568ca248efab32a063c745444c6e1f0bc64b25
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 13:43:35 +0800

    db calls

---
+++ PokemonTest/Repository/OwnerRepository.cs
@@ -0,0 +1,45 @@
+﻿using PokemonTest.Data;
+using PokemonTest.Interfaces;
+using PokemonTest.Models;
+
+namespace PokemonTest.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private DataContext _context;
+
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Owner> GetAllOwners()
+        {
+            return _context.Owners.ToList();
+        }
+
+        public Owner GetOwner(int id)
+        {
+           return _context.Owners.Where(o => o.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokemonId)
+        {
+            return _context.PokemonOwners
+                .Where(po=> po.Pokemon.Id == pokemonId)
+                .Select(o => o.Owner).ToList();  
+        }
+
+        public ICollection<Pokemon> GetPokemonsOfAnOwner(int ownerId)
+        {
+            return _context.PokemonOwners
+                .Where(po=> po.Owner.Id == ownerId)
+                .Select(p => p.Pokemon).ToList();
+        }
+
+        public bool OwnerExists(int id)
+        {
+           return _context.Owners.Any(o => o.Id == id);
+        }
+    }
+}
