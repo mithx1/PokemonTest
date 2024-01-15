bd76214c9a67f7cdb3dbe62e86730191f149aae1
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-16 07:32:57 +0800

    create mapping profile

---
+++ PokemonTest/Dto/ReviewerDto.cs
@@ -0,0 +1,12 @@
+﻿using PokemonTest.Models;
+
+namespace PokemonTest.Dto
+{
+    public class ReviewerDto
+    {        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
--- PokemonTest/Helpers/MappingProfiles.cs
+++ PokemonTest/Helpers/MappingProfiles.cs
@@ -12,6 +12,8 @@ namespace PokemonTest.Helpers
             CreateMap<Category, CategoryDto>();
             CreateMap<Country, CountryDto>();
             CreateMap<Owner, OwnerDto>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
         }
     }
 }
