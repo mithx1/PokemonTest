90a56a16e77cd351e10e412997cdc19c1d5cc62c
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-15 13:43:25 +0800

    Country and Owner interface

---
+++ PokemonTest/Interfaces/ICountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using PokemonTest.Models;
+
+namespace PokemonTest.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetAllCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        ICollection<Owner> GetOwnersFromACountry(int countryId);
+        bool CountryExists(int id);
+
+    
+    }
+}
---
+++ PokemonTest/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonTest.Models;
+
+namespace PokemonTest.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetAllOwners();
+        Owner GetOwner(int id);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokemonId);
+        ICollection<Pokemon> GetPokemonsOfAnOwner(int ownerId);
+        bool OwnerExists(int id);
+    
+    }
+}
