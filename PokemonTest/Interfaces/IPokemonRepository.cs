4cc9f1bdf6536822a2b6b8105522b86b0443837a
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-17 04:41:15 +0800

    add create pokemon

--- PokemonTest/Interfaces/IPokemonRepository.cs
+++ PokemonTest/Interfaces/IPokemonRepository.cs
@@ -9,5 +9,7 @@ namespace PokemonTest.Interfaces
         Pokemon GetPokemon(string name);
         decimal GetPokemonRating(int id);
         bool PokemonExists(int id);
+        bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon);
+        bool Save();
     }
 }
--- PokemonTest/Repository/PokemonRepository.cs
+++ PokemonTest/Repository/PokemonRepository.cs
@@ -12,6 +12,35 @@ namespace PokemonTest.Repository
         {
             _context = context;
         }
+
+        public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+        {
+            var pokemonOwnerEntity = _context.Owners.SingleOrDefault(a => a.Id == ownerId);
+            var pokemonCategoryEntity = _context.Categories.SingleOrDefault(a => a.Id == categoryId);
+
+            if (pokemonOwnerEntity == null || pokemonCategoryEntity == null)
+            {
+                return false;
+            }
+
+            var pokemonOwner = new PokemonOwner
+            {
+                Owner = pokemonOwnerEntity,
+                Pokemon = pokemon
+            };
+
+            var pokemonCategory = new PokemonCategory
+            {
+                Category = pokemonCategoryEntity,
+                Pokemon = pokemon
+            };
+
+            _context.AddRange(new object[] { pokemonOwner, pokemonCategory, pokemon });
+
+            return Save();
+        }
+
+
         public ICollection<Pokemon> GetAllPokemons()
         {
             return _context.Pokemons.OrderBy(p => p.Id).ToList();
@@ -42,6 +71,12 @@ namespace PokemonTest.Repository
         public bool PokemonExists(int id)
         {
             return _context.Pokemons.Any(p => p.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved >= 0 ? true : false;
         }
     }
 }
