ecff9372bd061ba202bcd83024d9bbb12bbdf5dd
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-19 00:31:18 +0800

    TODO update to use reversemap instead

--- PokemonTest/Helpers/MappingProfiles.cs
+++ PokemonTest/Helpers/MappingProfiles.cs
@@ -8,9 +8,14 @@ namespace PokemonTest.Helpers
     {
         public MappingProfiles()
         {
+            //TODO use reverse map instead of creating both maps
             CreateMap<Pokemon, PokemonDto>();
             CreateMap<PokemonDto, Pokemon>();
+            //TODO use reverse map instead of creating both maps
             CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+
+
             CreateMap<Country, CountryDto>();
             CreateMap<Owner, OwnerDto>();
             CreateMap<Review, ReviewDto>();
