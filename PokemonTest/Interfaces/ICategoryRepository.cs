8c04b540a28c4d10f48ef7912adcf2e58767d1ca
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-19 00:30:45 +0800

    add create methods in repository

--- PokemonTest/Interfaces/ICategoryRepository.cs
+++ PokemonTest/Interfaces/ICategoryRepository.cs
@@ -8,6 +8,7 @@ namespace PokemonTest.Interfaces
         Category GetCategory(int id);
         ICollection<Pokemon> GetPokemonByCategory(int categoryId);
         bool CategoryExists(int id);
-
+        bool CreateCategory(Category category);
+        bool Save();
     }
 }
